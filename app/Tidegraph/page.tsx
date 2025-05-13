51b44891cbd357dd3b464dfa8611e3aeed2e6a19
marumo333 <seito5026junior@icloud.com>
2025-05-13 15:12:58 +0900

    使用する関数を追加

--- app/Tidegraph/page.tsx
+++ app/Tidegraph/page.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import { PythonProvider } from 'react-py'
+import {useState,useEffect} from "react";
 export default function Tidegraph(){
+    const [pref,setPref] = useState("")//県を設定
+    const [port,setPort] = useState("")//港を設定
+    const [year,setYear] = useState(2025)//以下で年月日を設定
+    const [month,setMonth] = useState(1)
+    const [day,setDay] = useState(1)
+    const [data,setData] = useState<any>(null)//取得データを定義
+
+    
     return(
         <>
+
         </>
     )
 }
