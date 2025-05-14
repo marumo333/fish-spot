2f6e512489b5df684ce9fe8f59d0ba45ad38206b
marumo333 <seito5026junior@icloud.com>
2025-05-14 19:00:08 +0900

    コードのフォーマットを整形

--- app/Tidegraph/page.tsx
+++ app/Tidegraph/page.tsx
@@ -1,40 +1,41 @@
 import React from "react";
-import {useState,useEffect} from "react";
-export default function Tidegraph(){
-    const [pref,setPref] = useState("")//県を設定
-    const [port,setPort] = useState("")//港を設定
-    const [year,setYear] = useState(2025)//以下で年月日を設定
-    const [month,setMonth] = useState(1)
-    const [day,setDay] = useState(1)
-    const [data,setData] = useState<any>(null)//取得データを定義
-    const [pchc,setPchc] =useState<any>({})//港のデータ
-    const [pcCodes,setPcCodes] = useState<any>({})//都道府県データ
-    const [calender,setCalender] =useState({})//カレンダーデータ
+import { useState, useEffect } from "react";
+export default function Tidegraph() {
+  const [pref, setPref] = useState(""); //県を設定
+  const [port, setPort] = useState(""); //港を設定
+  const [year, setYear] = useState(2025); //以下で年月日を設定
+  const [month, setMonth] = useState(1);
+  const [day, setDay] = useState(1);
+  const [data, setData] = useState<any>(null); //取得データを定義
+  const [pchc, setPchc] = useState<any>({}); //港のデータ
+  const [pcCodes, setPcCodes] = useState<any>({}); //都道府県データ
+  const [calendar, setCalendar] = useState<{[key:string]:number[]}>({}); //カレンダーデータ
 
-    useEffect(()=>{
-        Promise.all([
-            fetch("./pc_hc.json").then(res=>res.json()),
-            fetch("./pc_code.json").then(res=>res.json()),
-            fetch("calender.json").then(res=>res.json())
-        ]).then(([hc,code,cal])=>{
-            setPchc(hc)
-            setPcCodes(code)
-            setCalender(cal)
-        })
-        .catch(err=>
-            console.error("データの取得に失敗しました",err)
-        )
-    },[])
-    const getTide=async()=>{
-        const pc = pcCodes[pref]
-        const hc = pchc[pref][port]
-        const res = await fetch(`https://api.tide736.net/get_tide.php?&pc=${pc}&hc=${hc}&yr=${year}&mh=${month}&dy=${day}&rg=day`)
-        const json = await res.json()
-        setData(json)
-    }
-    return(
-        <>
-        <button onClick={getTide}>情報を取得</button>
-        </>
-    )
+  useEffect(() => {
+    Promise.all([
+      fetch("./pc_hc.json").then((res) => res.json()),
+      fetch("./pc_code.json").then((res) => res.json()),
+      fetch("calender.json").then((res) => res.json()),
+    ])
+      .then(([hc, code, cal]) => {
+        setPchc(hc);
+        setPcCodes(code);
+        setCalendar(cal);
+      })
+      .catch((err) => console.error("データの取得に失敗しました", err));
+  }, []);
+  const getTide = async () => {
+    const pc = pcCodes[pref];
+    const hc = pchc[pref][port];
+    const res = await fetch(
+      `https://api.tide736.net/get_tide.php?&pc=${pc}&hc=${hc}&yr=${year}&mh=${month}&dy=${day}&rg=day`
+    );
+    const json = await res.json();
+    setData(json);
+  };
+  return (
+    <>
+      <main></main>
+    </>
+  );
 }
