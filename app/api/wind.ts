7aafc878a96cbccb017dc3bd546bda2b97d338c1
marumo333 <seito5026junior@icloud.com>
2025-05-17 17:31:54 +0900

    windy apiを追加

---
+++ app/api/wind.ts
@@ -0,0 +1,21 @@
+import type {NextApiRequest,NextApiResponse} from "next";
+export default async function handler(req:NextApiRequest,res:NextApiResponse){
+    const {lat,lon} = req.query;//latは緯度、lonは軽度
+    const apiKey = process.env.WINDY_API_KEY!;
+    const WindyRes = await fetch("https://api.windy.com/api/point-forecast/v2",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+            Authorization:`Bearer ${apiKey}`,
+        },
+        body:JSON.stringify({
+            lat:Number(lat),
+            lon:Number(lon),
+            model:"gfs",
+            parameters:["wind_u","wond_v"],
+            levels:["surface"],
+        })
+    });
+    const data = await WindyRes.json();
+    res.status(200).json(data);
+}
